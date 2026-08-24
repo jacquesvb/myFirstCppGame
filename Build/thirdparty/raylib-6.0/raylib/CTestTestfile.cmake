96a5a205356e4ee10638ed44d09e2204f194a81b
Jacques Van Blokland <jacquesvb@live.com>
2026-08-23 18:03:32 -0700

    Fixed gitignore

--- Build/thirdparty/raylib-6.0/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jacquesvb/Workspace/Cpp/Gamedev_Course/myFirstCppGame/thirdparty/raylib-6.0/src
-# Build directory: /home/jacquesvb/Workspace/Cpp/Gamedev_Course/myFirstCppGame/Build/thirdparty/raylib-6.0/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
